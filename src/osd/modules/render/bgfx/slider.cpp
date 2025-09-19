614c3624b9a2045019cefb1a78d7c87cd53796bc
R E Broadley <rebroad@users.noreply.github.com>
2025-09-19 09:19:06 +0200

    slider debug

--- src/osd/modules/render/bgfx/slider.cpp
+++ src/osd/modules/render/bgfx/slider.cpp
@@ -60,7 +60,7 @@ int32_t bgfx_slider::update(std::string *str, int32_t newval)
 {
 	if (newval != SLIDER_NOCHANGE)
 	{
-		osd_printf_verbose("BGFX Slider: %s changed to %d\n", m_description.c_str(), newval);
+		printf("BGFX Slider: %s changed to %d\n", m_description.c_str(), newval);
 	}
 	switch (m_type)
 	{
